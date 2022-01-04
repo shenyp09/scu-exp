5af5d6a3ca07b2c97b29beaa7914c27ce0ab2737
Fermi <fermi@Yang-Pings-Macbook-Pro.local>
2022-01-04 17:21:59 +0800

    init

---
+++ deal.C
@@ -0,0 +1,37 @@
+void deal() {
+	auto t = (TTree*)TFile::Open("data_R0004_W1000.root")->Get("t");
+
+	UShort_t adc[3][16];
+	Bool_t outofr[3][16];
+
+	TH1F *hbgo[8];
+	for (int i = 0; i < 8; ++i)
+	{
+		hbgo[i] = new TH1F(TString::Format("h%d", i), TString::Format("h%d", i), 3000, 0, 30000);
+	}
+
+	t->SetBranchAddress("adc", adc);
+	t->SetBranchAddress("outofr", outofr);
+
+	long nn = t->GetEntries();
+
+	for (long i = 0; i < nn / 100; ++i)
+	{
+		t->GetEntry(i);
+		for (int j = 0; j < 8; ++j)
+		{
+			hbgo[j]->Fill(adc[1][j + 8]);
+		}
+	}
+
+	TCanvas *c = new TCanvas();
+
+	c->Divide(2, 4);
+
+	for (int i = 0; i < 8; ++i)
+	{
+		c->cd(i + 1);
+		hbgo->Draw();
+	}
+
+}
